bf02364090777e92983b09f79c77f55357d8c05d
Flavie Meffo <flaviemeffo68@gmail.com>
2026-01-08 09:33:32 +0100

    configuration des models ok

--- BLOG/my_blog/admin.py
+++ BLOG/my_blog/admin.py
@@ -1,3 +1,24 @@
-from django.contrib import admin
+#from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Category, Post, Comment
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'pub_date', 'is_published')
+    list_filter = ('is_published', 'category')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'post', 'created_at', 'is_approved')
+    list_filter = ('is_approved',)
+    search_fields = ('name', 'message')
